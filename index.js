14ea9bff3f7154324e58f220a64d39bf22e03578
Alexandre Trovato <1839717+atrovato@users.noreply.github.com>
2019-12-29 10:41:43 +0100

    Add posibility to manage timer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const deviceTimer = require('./lib/deviceTimer.js');
+const timer = require('./lib/deviceTimer.js');
 
 module.exports = function (sails) {
   return {
-    deviceTimer: deviceTimer
+    deviceTimer: deviceTimer,
+    timerManager: timer
   };
 };
